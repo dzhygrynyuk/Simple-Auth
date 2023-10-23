088a01125636f73d0aa1682a3fd75cbb55bdc788
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-10-23 13:19:16 +0300

    Created roleMiddlaware

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -3,6 +3,7 @@ const { check } =  require('express-validator');
 
 const controller = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 
 const router = new Router();
 
@@ -11,6 +12,6 @@ router.post('/registration', [
     check('password', "Password must be longer than 4 characters and shorter than 10 characters").isLength({min: 4, max: 10})
 ], controller.registration);
 router.post('/login', controller.login);
-router.get('/users', authMiddleware, controller.getUsers);
+router.get('/users', roleMiddleware(['ADMIN']), controller.getUsers);
 
 module.exports = router;
