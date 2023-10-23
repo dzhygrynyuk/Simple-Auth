fdb3d01abee5cbbd2d97175504aa5d7b388f6451
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-10-23 13:36:01 +0300

    Setting .gitignone file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
+const { mongoDBPassword } = require('config');
 const authRouter = require('./routers/authRouter');
 
 const app = express();
@@ -11,7 +12,7 @@ app.use('/auth', authRouter);
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb+srv://dzhygrynyuk:pvUPlEEyhhlhtZmc@cluster0.cyzlkip.mongodb.net/Simple-Auth?retryWrites=true&w=majority');
+        await mongoose.connect(`mongodb+srv://dzhygrynyuk:${mongoDBPassword}@cluster0.cyzlkip.mongodb.net/Simple-Auth?retryWrites=true&w=majority`);
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (error) {
         console.log(error);
